f92af2f35315d4c7974acd3cc080e08ecd59b56e
Jakub Twardosz <jakubtwardosz@protonmail.com>
2024-08-01 22:40:01 +0200

    Card component

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { fakeData as notes } from "../assets/fakeData.js";
+import NoteCard from "../components/NoteCard.jsx";
 
 const NotesPage = () => {
-    return <div>NotesPage</div>;
+    return (
+        <div>
+            {notes.map((note) => (
+                <NoteCard note={note} key={note.$id} />
+            ))}
+        </div>
+    );
 }
 
 export default NotesPage;
